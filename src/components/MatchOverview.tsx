1087a49d45e46dd2cddbd514f5f28dd192eb1d40
Prayag <102691844+dystO2@users.noreply.github.com>
2023-05-11 20:47:30 +0530

    Update MatchOverview.tsx

--- src/components/MatchOverview.tsx
+++ src/components/MatchOverview.tsx
@@ -24,7 +24,7 @@ const MatchOverview = () => {
             <div className="flex flex-col justify-center">
               <User
                 size="xl"
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                src="https://images-ext-2.discordapp.net/external/sTSqvOscW2QoUHttc43vSzGpoTMw_kBln1iqGkHHsgI/%3Fu%3Dhttps%253A%252F%252Fsm.ign.com%252Fign_ap%252Fscreenshot%252Fdefault%252Frsglogo-full-colour_fu78.jpg%26f%3D1%26nofb%3D1%26ipt%3D06e4e99d2b005ea8a55a51cc3931001c3e7ec8fafac9c621f322ef4bef40bca4%26ipo%3Dimages/https/external-content.duckduckgo.com/iu/?width=1302&height=671"
                 name="RSG"
               />
             </div>
@@ -34,7 +34,7 @@ const MatchOverview = () => {
             <div className="flex flex-col justify-center">
               <User
                 size="xl"
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                src="https://images-ext-2.discordapp.net/external/HImTMZAFfTfalojf9y2VF4ROF11p-gK-cGYRU4DxmIw/%3Fu%3Dhttps%253A%252F%252Fsteamcdn-a.akamaihd.net%252Fsteamcommunity%252Fpublic%252Fimages%252Favatars%252F17%252F17b09d60c039dc177523aab3e95fb28599dc8695_full.jpg%26f%3D1%26nofb%3D1%26ipt%3Dc6c3a1fd1bc90909228c2689c3e10e5eb1470707b8ea212ff550855e2a9b0448%26ipo%3Dimages/https/external-content.duckduckgo.com/iu/"
                 name="69fps"
               />
             </div>
